71a0cc4ff38f98d7732322089e21f8e36b8b3678
Prema Tukaram Dongare <157959054+PremaDongare@users.noreply.github.com>
2025-01-06 23:58:21 +0530

    Time: 1 ms (99.76%), Space: 43.5 MB (68.8%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,28 +1,17 @@
 class Solution {
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> resultList = new ArrayList<>();
-        Arrays.sort(nums);
-
-        // start back tracking from the begining
-        backtrack(resultList, new ArrayList<>(), nums, 0);
-        return resultList;
-    }
-    private void backtrack(List<List<Integer>> resultSets, List<Integer> tempSet,int[] nums, int start ){
-        // if the set is alredy prensen , just continue
-        if(resultSets.contains((tempSet)))
-        return;
-
-        resultSets.add(new ArrayList<>(tempSet));
-
-        for(int i=start; i<nums.length; i++){
-            tempSet.add(nums[i]);
-
-            //  Backtrack the new subse
-            backtrack(resultSets, tempSet, nums, i+1);
-            // case of not include the number
-
-            tempSet.remove(tempSet.size()-1);
+    public void findSubsets(int ind, int[]nums, List<Integer> ds, List<List<Integer>>ansList){
+        ansList.add(new ArrayList<>(ds));
+        for(int i=ind; i<nums.length; i++){
+            if(i!=ind && nums[i] == nums[i-1]) continue;
+            ds.add(nums[i]);
+            findSubsets(i+1, nums, ds, ansList);
+            ds.remove(ds.size()-1);
         }
-
+    }
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ansList = new ArrayList<>();
+        findSubsets(0, nums, new ArrayList<>(), ansList);
+        return ansList;
     }
 }
