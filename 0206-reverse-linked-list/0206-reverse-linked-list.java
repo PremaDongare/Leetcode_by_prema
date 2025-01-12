a1df50c179ea5bb49d9c191e5ed2120159a3fdad
Prema Tukaram Dongare <157959054+PremaDongare@users.noreply.github.com>
2025-01-12 17:31:51 +0530

    Time: 0 ms (100%), Space: 42.5 MB (47.71%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -1,27 +1,32 @@
-import java.util.Stack;
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        // Stack to store the values of the linked list
-        Stack<Integer> valueStack = new Stack<>();
-        
-        // Traverse through the linked list and push values to the stack
-        while (head != null) {
-            valueStack.push(head.val);
-            head = head.next;
+        if(head == null){
+            return null;
         }
-
-        // Initialize the new reversed list
-        ListNode reversedList = new ListNode(Integer.MIN_VALUE);
-        ListNode ptr = reversedList;
-
-        // Pop the values from the stack and build the reversed linked list
-        while (!valueStack.isEmpty()) {
-            ptr.next = new ListNode(valueStack.pop());
-            ptr = ptr.next;
+        if(head.next == null){
+            return head;
         }
-        
-        // Return the reversed list
-        return reversedList.next;
+        ListNode preNode = null;
+        ListNode currNode = head;
+
+        while(currNode != null){
+            ListNode nextNode = currNode.next;
+            currNode.next = preNode;
+            preNode = currNode;
+            currNode = nextNode;
+        }
+        head = preNode;
+         return head;
     }
+        
 }
