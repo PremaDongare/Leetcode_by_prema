b0d1caed2e72c199268d3b2c36acebda44e67f84
Prema Tukaram Dongare <157959054+PremaDongare@users.noreply.github.com>
2025-06-04 22:13:15 +0530

    Time: 73 ms (5.56%), Space: 43.6 MB (98.15%) - LeetHub

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.java
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.java
@@ -1,19 +1,38 @@
 class Solution {
-    public String answerString(String word, int numFriends) {
-        int n = word.length();
-        if(numFriends == 1){
-            return word;
-        }
-        int longestpossible = n-(numFriends - 1);
-        String result =" ";
 
-        for(int i=0; i<n;i++){
-            int canTakeLength=Math.min(longestpossible , n-i);
-            String sub = word.substring(i, i+canTakeLength);
-            if(sub.compareTo(result)>0){
-                result =sub;
+    public int bestStartingPoint(String word, int n) {
+        int i = 0;
+        int j = 1;
+
+        while (j < n) {
+            int k = 0;
+
+            while (j + k < n && word.charAt(i + k) == word.charAt(j + k)) {
+                k++;
+            }
+
+            if (j + k < n && word.charAt(j + k) > word.charAt(i + k)) {
+                i = j;
+                j = j + 1;
+            } else {
+                j = j + k + 1;
             }
         }
-        return result;
+
+        return i;
+    }
+
+    public String answerString(String word, int numFriends) {
+        int n = word.length();
+
+        if (numFriends == 1) {
+            return word;
+        }
+
+        int i = bestStartingPoint(word, n);
+        int longestPossibleLength = n - (numFriends - 1);
+        int canTakePossible = Math.min(longestPossibleLength, n - i);
+
+        return word.substring(i, i + canTakePossible);
     }
 }
