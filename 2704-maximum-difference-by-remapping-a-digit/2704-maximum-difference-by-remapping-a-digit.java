712dc7fa43af01da89a54ef79d93bc17c2fa9bec
Prema Tukaram Dongare <157959054+PremaDongare@users.noreply.github.com>
2025-06-14 14:00:23 +0530

    Time: 1 ms (89.2%), Space: 41 MB (35.23%) - LeetHub

--- 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.java
+++ 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.java
@@ -1,27 +1,22 @@
 class Solution {
     public int minMaxDifference(int num) {
-        // convert num to strings
-        String minnum=Integer.toString(num);
-        String maxnum= Integer.toString(num);
+        String str1=Integer.toString(num);
+        String str2=str1;
 
-        int n = minnum.length();
-
-    // first digit in maximum that is not 9 replace it with 9
-
-    char ch = ' ';
-    for(int i=0; i<n;i++){
-        if(maxnum.charAt(i)!='9'){
-            ch=maxnum.charAt(i);
-            break;
+        int idx=0;
+        for(int i=0; i<str1.length();i++){
+            if(str1.charAt(i)!='9'){
+                idx=i;
+                break;
+            }
         }
-    }
-    if(ch != ' '){
-        maxnum=maxnum.replace(ch,'9');
-    }
-    // replace 1st digit with 0
-    ch= minnum.charAt(0);
-    minnum=minnum.replace(ch,'0');
 
-    return Integer.parseInt(maxnum) - Integer.parseInt(minnum);
+        if(idx<str1.length()){
+            str1=str1.replace(str1.charAt(idx),'9');
+        }
+
+        str2=str2.replace(str2.charAt(0),'0');
+
+        return Integer.parseInt(str1)-Integer.parseInt(str2);
     }
 }
