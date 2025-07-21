98f75129e54f8974cf8f1a4e233a2bca699f9f4e
Prema Tukaram Dongare <157959054+PremaDongare@users.noreply.github.com>
2025-07-21 23:00:43 +0530

    Time: 22 ms (35.11%), Space: 53.1 MB (6.86%) - LeetHub

---
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        Map<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+
+        int sum =0;
+        int count =0;
+
+        for(int num:nums){
+            sum += num;
+
+            if(map.containsKey(sum - goal)){
+                count+=map.get(sum-goal);
+            }
+                 //store the current prefix sum
+                 map.put(sum, map.getOrDefault(sum,0)+1);
+            
+        }
+        return count;
+    }
+}
