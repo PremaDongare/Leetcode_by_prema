29d82f617a59998c7651bfbefd359c1afbba7ee9
Prema Tukaram Dongare <157959054+PremaDongare@users.noreply.github.com>
2025-07-22 22:47:39 +0530

    Time: 7 ms (95.59%), Space: 55 MB (69.5%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
@@ -3,18 +3,18 @@ class Solution {
         int n = nums.length;
         int oddCount = 0;
         int result = 0;
-        int count =0;
+        int Prevcount =0;
 
         int i=0, j=0;
 
         while(j<n){
             if(nums[j] %2 != 0){
                 oddCount++;
-                count=0; // reset
+                Prevcount=0; // reset
             }
 
          while(oddCount == k){
-            count++;
+            Prevcount++;
 
             if(i<n && nums[i] % 2 == 1){
                 oddCount --;
@@ -23,7 +23,7 @@ class Solution {
             i++;
          }
 
-         result += count;
+         result += Prevcount;
          j++;
         }
         return result;
