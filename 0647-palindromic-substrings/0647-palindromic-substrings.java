1d2d1a1e9d21078569c0ff94a06f4a20164d4366
Prema Tukaram Dongare <157959054+PremaDongare@users.noreply.github.com>
2025-08-04 12:42:30 +0530

    Time: 32 ms (22.43%), Space: 49.6 MB (6.3%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,17 +1,31 @@
 class Solution {
-       //Brute force
+       //using memorization
+       int [][]t;
     public boolean check(String s, int i, int j){
         if(i>=j){
             return true;
         }
-
-        if(s.charAt(i)==s.charAt(j)){
-          return check(s, i+1, j-1);
+        if (t[i][j]!= -1){  // not visited
+            return t[i][j]==1; // visited
         }
-        return false;
+        if(s.charAt(i)==s.charAt(j)){
+         boolean val= check(s, i+1, j-1);
+         if(val==true){
+         t[i][j] =1;
+        }else{
+            t[i][j]=0;
+        }
+        return val;
+    }
+    t[i][j]=0;
+    return false;
     }
     public int countSubstrings(String s) {
        int n = s.length();
+       t= new int[n][n];
+       for(int[] row:t){
+        Arrays.fill(row,-1);
+       }
        int count =0;
 
        for(int i=0; i<n; i++){
