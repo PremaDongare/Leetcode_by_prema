b876d6a69c235b1b64cd7ed110ffb1faf13d7d13
Prema Tukaram Dongare <157959054+PremaDongare@users.noreply.github.com>
2026-02-12 23:53:46 +0530

    Time: 582 ms (27.61%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,3 +1,9 @@
-SELECT  DISTINCT  a.Num AS ConsecutiveNums FROM Logs a 
-JOIN logs b ON a.id = b.id+1 AND a.Num = b.Num
-JOIN logs c ON b.id = c.id+1 AND a.Num = c.Num 
+SELECT DISTINCT l1.num AS ConsecutiveNums
+FROM Logs l1
+JOIN Logs l2 
+    ON l1.id = l2.id - 1
+JOIN Logs l3 
+    ON l1.id = l3.id - 2
+WHERE l1.num = l2.num
+  AND l2.num = l3.num;
+
