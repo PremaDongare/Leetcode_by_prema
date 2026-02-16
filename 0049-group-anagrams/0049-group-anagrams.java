3837a2153c6333f0d6b81b7b84a26f971e65e89e
Prema Tukaram Dongare <157959054+PremaDongare@users.noreply.github.com>
2026-02-16 22:36:07 +0530

    Time: 19 ms (20.81%), Space: 50.7 MB (14.09%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,15 +1,25 @@
+// T.c O(n*k)
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String,List<String>> map = new HashMap<>();
+        HashMap<String, List<String>> map = new HashMap<>();
         for(String s:strs){
-            char[]arr = s.toCharArray();
-            Arrays.sort(arr);
-            String key = new String(arr);
+         int[] freq = new int[26];
 
-            map.putIfAbsent(key, new ArrayList<>());
-            map.get(key).add(s);
+         for(char c:s.toCharArray()){
+            freq[c -'a']++;
+         }
+
+         StringBuilder key = new StringBuilder();
+         for(int count : freq){
+            key.append(count).append('#');
+         }
+         map.putIfAbsent(key.toString(), new ArrayList<>());
+         map.get(key.toString()).add(s);
         }
 
         return new ArrayList<>(map.values());
+       
+       
+        
     }
 }
