d8f020f56909117c8fb2e1985beee0482bb46707
Prema Tukaram Dongare <157959054+PremaDongare@users.noreply.github.com>
2026-02-17 19:29:17 +0530

    Time: 969 ms (22.63%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,9 +1,12 @@
-SELECT d.name AS Department,
-       e.name AS Employee,
-       e.salary AS Salary
-FROM Employee e
-JOIN Department d
-   ON e.departmentId = d.id
-WHERE e.salary=(SELECT MAX(salary)
-FROM Employee 
-WHERE departmentId = e.departmentID);
+# using join and subquery
+select d.name as Department, e.name as Employee, e.salary as Salary
+from employee e
+join Department d
+on e.departmentId = d.id
+# subquery
+where e.salary=(
+    select max(e2.salary)
+    from Employee e2
+    where e2.departmentId = e.departmentId
+)
+
