739ecfc7ca508c37d82b9b6c5d824a53014d9267
Prema Tukaram Dongare <157959054+PremaDongare@users.noreply.github.com>
2026-02-18 19:22:27 +0530

    Time: 633 ms (63.84%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,12 +1,13 @@
-# using join and subquery
+# By joins and Group by
 select d.name as Department, e.name as Employee, e.salary as Salary
-from employee e
-join Department d
+from Employee e
+join Department d 
 on e.departmentId = d.id
-# subquery
-where e.salary=(
-    select max(e2.salary)
-    from Employee e2
-    where e2.departmentId = e.departmentId
-)
+join(
+    select departmentId, max(salary) As maxsalary
+    From Employee
+    Group by departmentId
+)temp         #store max salary
+on e.departmentId = temp.departmentId
+And e.salary = temp.maxSalary;
 
