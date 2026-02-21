6f0c5a2e410a4d5f17c425d0716377f9f269ebea
Prema Tukaram Dongare <157959054+PremaDongare@users.noreply.github.com>
2026-02-21 22:42:22 +0530

    Time: 5 ms (97.18%), Space: 42.8 MB (92.28%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,27 +1,30 @@
+
+// Expand around center 
+//Tc = O(n^2)
+//Sc= O(1)
+
 class Solution {
+
+     int count =0;
+
+    private void check(String s, int i, int j, int n){
+        while(i>=0 && j<n && s.charAt(i) == s.charAt(j)){
+            count ++;
+// expand from center
+            i--;
+            j++;         
+        }
+    }
     public int countSubstrings(String s) {
-        int n=s.length();
-        boolean [][]t = new boolean[n][n];
-        int count =0;
-        // length of substring start with 1
-        for(int L=1; L<=n; L++){
-        for(int i=0; i+L<=n; i++){
-            int j=i+L-1;
+        int n = s.length();
+         count =0;
 
-            // for 1 length single character
-            if(i==j){
-                t[i][i]=true;
-            }else if (i+1 == j){
-               t[i][j] = (s.charAt(i) == s.charAt(j));  // string of two character
-            }else{
-                t[i][j] = (s.charAt(i) == s.charAt(j) && t[i+1][j-1]);
-            }
+        // consider every single character as a center .
+              for(int i=0; i<n; i++){
+                check(s,i,i,n); // odd
+                check(s, i, i+1, n);//even
+              }
+              return count;
 
-            if(t[i][j]){
-                count ++;
-            }
-        }
-        }
-        return count;
     }
 }
